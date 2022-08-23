cbf08e87a2d33f4e7b7542caab4519b3da4f88f9
FSArriagada <facu._99@hotmail.com>
2022-08-23 17:45:29 -0300

    Portfolio

---
+++ index.js
@@ -0,0 +1,7 @@
+ScrollReveal().reveal('.about', { delay: 500 });
+ScrollReveal().reveal('.skills', { delay: 1000 });
+ScrollReveal().reveal('.projects', { delay: 800 });
+ScrollReveal().reveal('.card1', { delay: 900 });
+ScrollReveal().reveal('.card2', { delay: 1000 });
+ScrollReveal().reveal('.card3', { delay: 1100 });
+ScrollReveal().reveal('.footer', { delay: 1000 });
